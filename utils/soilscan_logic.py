5cf83ff721a93c173bfdf7f80a43175c02c4f63f
29anirudh29 <108411@nd.balbharati.org>
2025-10-26 12:45:15 +0530

    Update soilscan_logic.py

--- utils/soilscan_logic.py
+++ utils/soilscan_logic.py
@@ -24,4 +24,26 @@ def analyze_soil(image_path):
     NASA POWER data is completely removed.
     """
     soil_params = approximate_soil_from_image(image_path)
-    return soil_params, None  # No plot
+    
+    # --- Recommended crops logic ---
+    crops = []
+    clay_high = soil_params['Clay'] > 40
+    silt_high = soil_params['Silt'] > 40
+    sand_high = soil_params['Sand'] > 40
+
+    if clay_high and not (silt_high or sand_high):
+        crops = ['Broccoli', 'Cabbage', 'Rice']
+    elif silt_high and not (clay_high or sand_high):
+        crops = ['Pea', 'Wheat', 'Potato']
+    elif sand_high and not (clay_high or silt_high):
+        crops = ['Jowar', 'Bajra', 'Maize']
+    elif clay_high and silt_high:
+        crops = ['Rice', 'Potato', 'Wheat']
+    elif silt_high and sand_high:
+        crops = ['Potato', 'Jowar', 'Bajra']
+    elif clay_high and sand_high:
+        crops = ['Jowar', 'Bajra', 'Wheat']
+    else:
+        crops = ['Pea', 'Wheat', 'Potato', 'Broccoli', 'Cabbage', 'Rice', 'Jowar', 'Bajra', 'Maize']  # fallback mix
+
+    return soil_params, crops  # return crops along with soil params
